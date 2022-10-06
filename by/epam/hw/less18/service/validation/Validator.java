e7c3d2a719a40f00e4d9240a1674b398a6410d23
TatsianaMalashankava <109862123+TanyaM3@users.noreply.github.com>
2022-10-06 14:53:10 +0200

    Update Validator.java

--- by/epam/hw/less18/service/validation/Validator.java
+++ by/epam/hw/less18/service/validation/Validator.java
@@ -4,8 +4,6 @@ import by.epam.hw.less18.entity.criteria.Criteria;
 
 public class Validator {
 	public static boolean criteriaValidator(Criteria criteria) {
-		// you may add your own code here
-
 		return true;
 	}
 }
